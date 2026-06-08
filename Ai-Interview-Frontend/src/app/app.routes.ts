d0848d9e132f9606629bb95d216775e0f516a50a
shubhamchaudhari <shubhamchaudhari9990@gmail.com>
2026-06-08 17:27:22 +0530

    fix: auth guard and zone.js

--- Ai-Interview-Frontend/src/app/app.routes.ts
+++ Ai-Interview-Frontend/src/app/app.routes.ts
@@ -7,55 +7,12 @@ export const routes: Routes = [
     pathMatch: 'full',
   },
   {
-    path: 'about-Us',
-    loadComponent: () =>
-      import('./features/dashboard/pages/about-us/about-us').then((m) => m.AboutUs),
-  },
-  {
     path: 'auth',
     loadChildren: () => import('./features/auth/auth.routes').then((m) => m.authRoutes),
   },
   {
-    path: 'blog',
-    loadComponent: () => import('./features/dashboard/pages/blog/blog').then((m) => m.Blog),
-  },
-  {
-    path: 'blog-listing',
-    loadComponent: () =>
-      import('./features/dashboard/pages/blog-listing/blog-listing').then((m) => m.BlogListing),
-  },
-  {
-    path: 'careers',
-    loadComponent: () =>
-      import('./features/dashboard/pages/careers/careers').then((m) => m.Careers),
-  },
-  {
-    path: 'contact-us',
-    loadComponent: () =>
-      import('./features/dashboard/pages/contact-us/contact-us').then((m) => m.ContactUs),
-  },
-  {
-    path: 'enterprise',
-    loadComponent: () =>
-      import('./features/dashboard/pages/enterprise/enterprise').then((m) => m.Enterprise),
-  },
-  {
-    path: 'faq',
-    loadComponent: () => import('./features/dashboard/pages/faq/faq').then((m) => m.Faq),
-  },
-  {
-    path: 'features',
-    loadComponent: () =>
-      import('./features/dashboard/pages/features/features').then((m) => m.Features),
-  },
-  {
-    path: 'home',
-    loadComponent: () =>
-      import('./features/dashboard/pages/home/home').then((m) => m.HomeComponent),
-  },
-  {
-    path: 'pricing',
-    loadComponent: () =>
-      import('./features/dashboard/pages/pricing/pricing').then((m) => m.Pricing),
+    path: '',
+    loadChildren: () =>
+      import('./features/dashboard/dashboard.routes').then((m) => m.dashboardRoutes),
   },
 ];
