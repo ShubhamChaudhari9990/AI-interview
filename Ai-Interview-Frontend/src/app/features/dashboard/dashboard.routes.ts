c7a9bcc028ebe01145cad10fa1c001da3a5d5ff1
ashishgupta09 <ashishgupta95652@gmail.com>
2026-06-09 16:12:02 +0530

    fixed the routing issues.

--- Ai-Interview-Frontend/src/app/features/dashboard/dashboard.routes.ts
+++ Ai-Interview-Frontend/src/app/features/dashboard/dashboard.routes.ts
@@ -5,51 +5,55 @@ export const dashboardRoutes: Routes = [
   {
     path: 'landing',
     canActivate: [AuthGuard],
-    loadComponent: () => import('./pages/landing-page/landing-page').then((m) => m.LandingPage),
+    loadComponent: () =>
+      import('./../landing/pages/landing-page/landing-page').then((m) => m.LandingPage),
   },
   {
     path: 'about-us',
     canActivate: [AuthGuard],
-    loadComponent: () => import('./pages/about-us/about-us').then((m) => m.AboutUs),
+    loadComponent: () => import('./../landing/pages/about-us/about-us').then((m) => m.AboutUs),
   },
   {
     path: 'blog',
     canActivate: [AuthGuard],
-    loadComponent: () => import('./pages/blog/blog').then((m) => m.Blog),
+    loadComponent: () => import('./../landing/pages/blog/blog').then((m) => m.Blog),
   },
   {
     path: 'blog-listing',
     canActivate: [AuthGuard],
-    loadComponent: () => import('./pages/blog-listing/blog-listing').then((m) => m.BlogListing),
+    loadComponent: () =>
+      import('./../landing/pages/blog-listing/blog-listing').then((m) => m.BlogListing),
   },
   {
     path: 'careers',
     canActivate: [AuthGuard],
-    loadComponent: () => import('./pages/careers/careers').then((m) => m.Careers),
+    loadComponent: () => import('./../landing/pages/careers/careers').then((m) => m.Careers),
   },
   {
     path: 'contact-us',
     canActivate: [AuthGuard],
-    loadComponent: () => import('./pages/contact-us/contact-us').then((m) => m.ContactUs),
+    loadComponent: () =>
+      import('./../landing/pages/contact-us/contact-us').then((m) => m.ContactUs),
   },
   {
     path: 'enterprise',
     canActivate: [AuthGuard],
-    loadComponent: () => import('./pages/enterprise/enterprise').then((m) => m.Enterprise),
+    loadComponent: () =>
+      import('./../landing/pages/enterprise/enterprise').then((m) => m.Enterprise),
   },
   {
     path: 'faq',
     canActivate: [AuthGuard],
-    loadComponent: () => import('./pages/faq/faq').then((m) => m.Faq),
+    loadComponent: () => import('./../landing/pages/faq/faq').then((m) => m.Faq),
   },
   {
     path: 'features',
     canActivate: [AuthGuard],
-    loadComponent: () => import('./pages/features/features').then((m) => m.Features),
+    loadComponent: () => import('./../landing/pages/features/features').then((m) => m.Features),
   },
   {
     path: 'pricing',
     canActivate: [AuthGuard],
-    loadComponent: () => import('./pages/pricing/pricing').then((m) => m.Pricing),
+    loadComponent: () => import('./../landing/pages/pricing/pricing').then((m) => m.Pricing),
   },
 ];
